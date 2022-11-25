563b31336680f2b6d222357a017af81ecc72aa94
6.6 <2213936042@qq.com>
2022-11-25 19:53:14 +0800

    config

---
+++ config.h
@@ -0,0 +1,29 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#include "webserver.h"
+
+using namesapce std;
+
+class Config
+{
+public:
+    Config();
+    ~Config();
+    void parse_argh(int argc, char* argv[]);
+
+    int PORT;
+    int LOGWrite; //日志同步/异步
+    int TRIGMode; //触发组合模式
+
+    int LISTENTrigmode; //listenfd触发方式
+    int CONNTrigmode; //connfd触发方式
+
+    int OPT_LINGER; //优雅关闭连接
+    int sql_num;
+    int thread_num;
+
+    int close_log; //是否关闭日志
+    int actor_model; //并发模型reactor/proactor
+};
+#endif
